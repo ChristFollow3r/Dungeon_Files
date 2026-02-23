682409edd848bb903e5d6b23fcc5b84e03eab559
Nil <nil.badia@estudiant.enti.cat>
2026-02-23 17:37:33 +0100

    Added briefs to gameplay.h

--- dungeon/gameplay.h
+++ dungeon/gameplay.h
@@ -8,7 +8,22 @@ struct Position {
 	int x;
 	int y;
 };
-
+/// <summary>
+/// Adds points (currency) and adds to the winChance variable
+/// </summary>
+/// <param name="points">The amount of money the player has</param>
+/// <param name="winChance">The chances the player has to win a combat</param>
+void AddPoints(int& points, int& winChance);
+/// <summary>
+/// Allows the player to escape the dungeon without fighting
+/// </summary>
+/// <param name="points">The amount of money the player has</param>
+void EscapeDungeon(int& points);
+/// <summary>
+/// Randomly places a 'P' inside the dungeon if the player exits the shop
+/// </summary>
+/// <param name="dungeon"></param>
+void FindShop(std::vector<std::vector<char>>& dungeon);
 /// <summary>
 /// Move the player inside the dungeon and other stuff (like getting money from chests)
 /// </summary>
